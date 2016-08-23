ff28cc0cc83ecd58ffafcb660707e2962f45cd52
jinkimdata <jinkimdata@gmail.com>
2016-08-23 17:00:31 -0400

    first commit

--- dist/js/app.js
+++ dist/js/app.js
@@ -2949,29 +2949,36 @@
     }, b || (a.jQuery = a.$ = r), r;
 });
 
-var dataproject = {
-    init: function() {},
-    share: function() {
-        $(".icon-twitter").on("click", function() {
-            var tweet = "";
-            var url = "";
-            var twitter_url = "https://twitter.com/intent/tweet?text=" + tweet + "&url=" + url + "&tw_p=tweetbutton";
-            window.open(twitter_url, "mywin", "left=200,top=200,width=500,height=300,toolbar=1,resizable=0");
-            return false;
-        });
-        $(".icon-facebook").on("click", function() {
-            var picture = "";
-            var title = "";
-            var description = "";
-            var url = "";
-            var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link=" + url + "&picture=" + picture + "&name=" + title + "&description=" + description + "&redirect_uri=http://www.facebook.com";
-            window.open(facebook_url, "mywin", "left=200,top=200,width=500,height=300,toolbar=1,resizable=0");
-            return false;
+var stkCities = {
+    init: function() {
+        stkCities.citiesScrollSnap();
+    },
+    citiesScrollSnap: function() {
+        var cities = $(".cityOutline");
+        var animating = false;
+        $(window).scroll(function() {
+            clearTimeout($.data(this, "scrollTimer"));
+            if (!animating) {
+                $.data(this, "scrollTimer", setTimeout(function() {
+                    cities.each(function(key, value) {
+                        if ($(value).offset().top > $(window).scrollTop()) {
+                            animating = true;
+                            $("body").animate({
+                                scrollTop: $(value).offset().top + "px"
+                            }, 250);
+                            setTimeout(function() {
+                                animating = false;
+                            }, 300);
+                            return false;
+                        }
+                    });
+                }, 200));
+            }
         });
     }
 };
 
 $(document).ready(function() {
-    dataproject.init();
+    stkCities.init();
     console.log("connected");
 });
